e6145248ae9c01832ef45a976bb387ba9611db59
Wes Kruger <wesleykruger@gmail.com>
2019-07-09 21:26:40 -0500

    adding event template

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -8,7 +8,11 @@ const MyCalendar = props => (
   <div>
     <Calendar
       localizer={localizer}
-      events={[
+      events={[{
+        title: 'test',
+        start: new Date("2019-07-09T10:00:00Z"),
+        end: new Date("2019-07-09T12:00:00Z")
+      }
       ]}
       startAccessor="start"
       endAccessor="end"
